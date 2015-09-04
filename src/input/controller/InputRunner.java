568923d3190815562a0c232d83c138877a270f42
smartman97 <smartman97@gmail.com>
2015-09-04 09:42:04 -0600

    Add project to repo.
    My first project added to Github!

---
+++ src/input/controller/InputRunner.java
@@ -0,0 +1,12 @@
+package input.controller;
+
+public class InputRunner
+{
+
+	public static void main(String[] args)
+	{
+		InputController myInputController = new InputController();
+		myInputController.start();
+	}
+
+}
